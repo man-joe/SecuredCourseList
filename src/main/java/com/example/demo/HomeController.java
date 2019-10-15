76d4ba8d107c98d53d6a2ecca1c70dc8e6ccabbc
man-joe <jhoang3@gmail.com>
2019-10-15 15:17:01 -0400

    secured course list

---
+++ src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,95 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.security.Principal;
+
+@Controller
+public class HomeController {
+    @Autowired UserService userService;
+    @Autowired UserRepository userRepository;
+    @Autowired CourseRepository courseRepository;
+
+    @GetMapping("/register")
+    public String showRegistrationPage(Model model){
+        model.addAttribute("user", new User());
+        return "registration";
+    }
+
+    @PostMapping("/register")
+    public String processRegistrationPage(@Valid @ModelAttribute("user") User user, BindingResult result, Model model){
+        model.addAttribute("user", user);
+        if(result.hasErrors())
+            return "registration";
+        else {
+            userService.saveUser(user);
+            model.addAttribute("message", "User Account Created");
+        }
+        return "index";
+    }
+
+//    @RequestMapping("/index")
+//    public String index(){
+//        return "index";
+//    }
+
+    @RequestMapping("login")
+    public String login(){
+        return "login";
+    }
+//    @RequestMapping("/admin")
+//    public String admin(){
+//        return "admin";
+//    }
+
+    @RequestMapping("/secure")
+    public String secure(Principal principal, Model model){
+        String username = principal.getName();
+        model.addAttribute("user", userRepository.findByUsername(username));
+        return "secure";
+    }
+
+    @RequestMapping("/")
+    public String listCourses(Model model){
+        model.addAttribute("courses", courseRepository.findAll());
+        return "list";
+    }
+
+    @GetMapping("/add")
+    public String courseForm(Model model){
+        model.addAttribute("course", new Course());
+        return "courseform";
+    }
+
+    @PostMapping("/process")
+    public String processForm(@Valid Course course, BindingResult result){
+        if(result.hasErrors()){
+            return "courseform";
+        }
+        courseRepository.save(course);
+        return "redirect:/";
+    }
+
+    @RequestMapping("/detail/{id}")
+    public String showCourse(@PathVariable("id") long id, Model model){
+        model.addAttribute("course", courseRepository.findById(id).get());
+        return "show";
+    }
+
+    @RequestMapping("/update/{id}")
+    public String updateCourse(@PathVariable("id") long id, Model model){
+        model.addAttribute("course", courseRepository.findById(id).get());
+        return "courseform";
+    }
+
+    @RequestMapping("/delete/{id}")
+    public String delCourse(@PathVariable("id") long id){
+        courseRepository.deleteById(id);
+        return "redirect:/";
+    }
+}
